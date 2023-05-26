22f55988628193dd5dd53befb8decc3ea6fe51e3
JOSE VARGAS¡¡ <joseroberto-vargas@hotmail.com>
2023-05-26 08:44:47 -0500

    solucionando el problema de las galerias

--- src/pages/galerias/Galeria.jsx
+++ src/pages/galerias/Galeria.jsx
@@ -1,86 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Component } from "react";
 import { getStorage, ref, getDownloadURL, listAll } from "firebase/storage";
 import firebaseApp from "../../firebase";
 import { Carousel } from "react-bootstrap";
 import { useState } from "react";
-import '../galerias/Galeria.css'
+import "../galerias/Galeria.css";
 
-const Galeria = () => {
+const storage = getStorage(firebaseApp);
+const gsReference = ref(storage, "gs://etno-latir.appspot.com/arte/");
+let pictures = [];
 
-  const storage = getStorage(firebaseApp);
-  const gsReference = ref(storage, "gs://etno-latir.appspot.com/arte/");
-  const arrayimg = [];
-  // const [files, setFiles] = useState();
+export default class Galeria extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      files: [],
+      load: false,
+    };
+  }
 
-  //   const load = false;
+  componentDidMount() {
+    this.load();
+  }
 
-  //   arrayimg.push(
-  //     "https://firebasestorage.googleapis.com/v0/b/etno-latir.appspot.com/o/arte%2F75e3188a-e67b-40a2-a117-f026a2075f9b?alt=media&token=70f808a5-683f-4452-8eeb-35cff7d60878"
-  //   );
-
-
-
-  listAll(gsReference)
-    .then((res) => {
-      /*res.prefixes.forEach((folderRef) => {
-          // All the prefixes under listRef.
-          // You may call listAll() recursively on them.
-        });*/
-      res.items.map((itemRef) => {
-        getDownloadURL(ref(storage, itemRef._location.path_))
-          .then((url) => {
-            // `url` is the download URL for 'images/stars.jpg'
-            //   "https://img.freepik.com/foto-gratis/sedan-deportivo-azul-estacionado-patio_114579-5078.jpg";
-            // setFiles([...test]);
-            // setLoad(true);
-            //  console.log("imagen google", url);
-            //this.setState({ files: [...this.state.files, url] });
-            //setPictures((files) => [...files, url]);
-
-            arrayimg.push(url);
-
-            // console.log("ver", arrayimg);
-
-            // setFiles(arrayimg)
-            // load = true;
-          })
-          .catch((error) => {
-            // Handle any errors
-          });
+  load = async () => {
+    await listAll(gsReference).then((res) => {
+      res.items.map(async (itemRef) => {
+        await getDownloadURL(ref(storage, itemRef.location.path)).then(
+          (url) => {
+            pictures.push(url);
+            this.setState({
+              load: true,
+            });
+            this.setState((prevState) => ({
+              files: [...prevState.files, url],
+            }));
+            console.log(this.state.files);
+          }
+        );
       });
-   
-      //   arrayimg.forEach((row) => {
-      //     console.log("array", row);
-      //     /*item.forEach((row) => {
-      //         dataImg.push(row);
-      //       });*/
-      //   });
-    })
-
-    .catch((error) => {
-      // Uh-oh, an error occurred!
     });
-    console.log("ver", arrayimg);
+  };
+
+  render() {
     return (
-      <div className='galeria'>
-            <Carousel  variant="dark" >
-                {
-                    arrayimg?.map((img, index) => (
-
-                        <Carousel.Item key={index}  >
-                            <img
-                                className="img-galeria"
-                                src={img}
-                                alt="First slide"
-                            />
-                        </Carousel.Item>
-                    ))
-                }
-            </Carousel>
-        </div>
+      <div className="galeria">
+        <Carousel>
+          {this.state.load &&
+            this.state.files?.map((image, idx) => (
+              <Carousel.Item>
+                <img className="d-block w-100" src={image} key={idx} />
+              </Carousel.Item>
+            ))}
+        </Carousel>
+      </div>
     );
-};
-
-export default Galeria;
-
-
+  }
+}
